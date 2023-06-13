f14caaae0c0554289cc20301b624ea3762d68b54
Leonardo8221 <r.leonardo0802@gmail.com>
2023-06-13 17:47:14 +0300

    Complete Sign In, Sign Out functions

---
+++ src/components/home/index.js
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getCurrentProfile, deleteAccount } from '../../actions/profile';
+
+const Home = ({
+  getCurrentProfile,
+  deleteAccount,
+  auth: { user },
+  profile: { profile }
+}) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, [getCurrentProfile]);
+
+  return (
+    <section className="container">
+      <h1 className="large text-primary">Home</h1>
+
+      <p>You have not yet setup a profile, please add some info</p>
+      <Link to="/create-profile" className="btn btn-primary my-1">
+        Create Profile
+      </Link>
+    </section>
+  );
+};
+
+Home.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  deleteAccount: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  profile: state.profile
+});
+
+export default connect(mapStateToProps, { getCurrentProfile, deleteAccount })(
+  Home
+);
