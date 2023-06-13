872487306b727506364d9a94bb05be15986251ef
Leonardo8221 <r.leonardo0802@gmail.com>
2023-06-13 23:03:40 +0300

    Complete GameSetup component

--- src/components/Game/GameSetup.js
+++ src/components/Game/GameSetup.js
@@ -1,13 +1,21 @@
+import { useNavigate } from 'react-router-dom';
 import './style.css';
 const GameSetup = () => {
+
+    const navigated = useNavigate()
+const handleDoneBtn = (e) => {
+    e.target.disabled = true;
+    navigated('/gameRoom');
+}
+
   return (
-    <section className="container">
+    <section className="container p-2">
       <h2 className="text-primary mb-4">Game Setup</h2>
       <div className="game-players mb-4">
         <div className="game-player">
           <div className="user-icon">
             <i className="fa fa-user"></i>
-            <span >User1</span>
+            <span>User1</span>
           </div>
           <div className="caption">Building Portfolio...</div>
         </div>
@@ -21,18 +29,48 @@ const GameSetup = () => {
         <div className="game-player">
           <div className="user-icon">
             <i className="fa fa-user"></i>
-            <span >User3</span>
+            <span>User3</span>
           </div>
           <div className="caption">Building Portfolio...</div>
         </div>
       </div>
 
-      <div className='game-setup-container'>
-        <div className='left'>
-
+      <div className="game-setup-container">
+        <div className="left p-4">
+          <h4>Stock List</h4>
+          <div className="stock-list">
+            <div className='stock-list-item'>Stock1</div>
+            <div className='stock-list-item'>Stock2</div>
+            <div className='stock-list-item'>Stock3</div>
+            <div className='stock-list-item'>Stock4</div>
+            <div className='stock-list-item'>Stock5</div>
+            <div className='stock-list-item'>Stock6</div>
+            <div className='stock-list-item'>Stock7</div>
+            <div className='stock-list-item'>Stock8</div>
+            <div className='stock-list-item'>Stock9</div>
+            <div className='stock-list-item'>Stock10</div>
+            <div className='stock-list-item'>Stock11</div>
+          </div>
         </div>
-        <div className='right'>
-
+        <div className="right p-4">
+          <div className="d-flex flex-row justify-content-between align-items-center mb-3">
+            <div className="d-flex flex-row justify-content-between align-items-center">
+              <p className="large text-primary mb-0">
+                Budget: <span className="large text-success">$1000</span>
+              </p>
+            </div>
+            <button className="btn btn-success" onClick={handleDoneBtn}>Done</button>
+          </div>
+          <div className="selected-stocks">
+            <div className="selected-stocks-item">Stock2</div>
+            <div className="selected-stocks-item">Stock3</div>
+            <div className="selected-stocks-item">Stock1</div>
+            <div className="selected-stocks-item">Stock4</div>
+            <div className="selected-stocks-item">Stock1</div>
+            <div className="selected-stocks-item">Stock4</div>
+            <div className="selected-stocks-item">Stock1</div>
+            <div className="selected-stocks-item">Stock4</div>
+          </div>
         </div>
       </div>
     </section>
