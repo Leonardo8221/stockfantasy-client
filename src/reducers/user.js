f4a39f12c68bb0d9b6a665e36cf57f8a2ee0a9d8
Leonardo8221 <r.leonardo0802@gmail.com>
2023-06-16 17:05:31 +0300

    Fix users reducer

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,26 +1,24 @@
 import {
-    GET_ALL_USERS_REQUEST,
-    GET_ALL_USERS_REQUEST_SUCCESS,
-    GET_ALL_USERS_REQUEST_ERROR,
-  } from "../constants/userConstant";
-  
-  const initialState = {
-    users: [],
-    loading: false,
-    error: null,
-    isLoadedUsers: false,
-  };
-  
-  export const getAllUsersReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case GET_ALL_USERS_REQUEST:
-        return { ...state, loading: true };
-      case GET_ALL_USERS_REQUEST_SUCCESS:
-        return { ...state, loading: false, users: action.payload, isLoadedUsers: true};
-      case GET_ALL_USERS_REQUEST_ERROR:
-        return { ...state, loading: false, error: action.payload };
-      default:
-        return state;
-    }
-  };
-  
+  GET_ALL_USERS_REQUEST,
+  GET_ALL_USERS_REQUEST_SUCCESS,
+  GET_ALL_USERS_REQUEST_ERROR,
+} from "../constants/userConstant";
+
+const initialState = {
+  users: [],
+  loading: false,
+  error: null,
+};
+
+export const getAllUsersReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_ALL_USERS_REQUEST:
+      return { ...state, loading: true };
+    case GET_ALL_USERS_REQUEST_SUCCESS:
+      return { ...state, loading: false, users: action.payload };
+    case GET_ALL_USERS_REQUEST_ERROR:
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
