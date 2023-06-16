6a65d13d3ad706c3b614cbd4da6da6d59fdf339a
Leonardo8221 <r.leonardo0802@gmail.com>
2023-06-16 10:05:58 +0300

    Add get all users reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import alert from './alert';
 import auth from './auth';
 import {roomCreateReducer} from './room';
+import { getAllUsersReducer } from './user';
 
 export default combineReducers({
   alert,
   auth,
   roomCreateReducer,
+  getAllUsersReducer
 });
---
+++ src/reducers/user.js
@@ -0,0 +1,25 @@
+import {
+    GET_ALL_USERS_REQUEST,
+    GET_ALL_USERS_REQUEST_SUCCESS,
+    GET_ALL_USERS_REQUEST_ERROR,
+  } from "../constants/userConstant";
+  
+  const initialState = {
+    users: {},
+    loading: false,
+    error: null,
+  };
+  
+  export const getAllUsersReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case GET_ALL_USERS_REQUEST:
+        return { ...state, loading: true };
+      case GET_ALL_USERS_REQUEST_SUCCESS:
+        return { ...state, loading: false, users: action.payload };
+      case GET_ALL_USERS_REQUEST_ERROR:
+        return { ...state, loading: false, error: action.payload };
+      default:
+        return state;
+    }
+  };
+  
