bc872fa6a3c88ec705b8fadaaef9276800fd8275
Leonardo8221 <r.leonardo0802@gmail.com>
2023-06-21 00:56:16 +0300

    Add getScores action

--- src/actions/score.js
+++ src/actions/score.js
@@ -6,6 +6,9 @@ import {
   GIVE_SCORE_REQUEST,
   GIVE_SCORE_REQUEST_ERROR,
   GIVE_SCORE_REQUEST_SUCCESS,
+  GET_SCORES_REQUEST,
+  GET_SCORES_REQUEST_ERROR,
+  GET_SCORES_REQUEST_SUCCESS,
 } from "../constants/scoreConstant";
 
 export const giveScoreToUser = (formData) => async (dispatch) => {
@@ -15,6 +18,26 @@ export const giveScoreToUser = (formData) => async (dispatch) => {
     const { data } = await api.post("/scores", formData);
 
     dispatch({ type: GIVE_SCORE_REQUEST_SUCCESS, payload: data });
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    if (message === "Not authorized, token failed") {
+      dispatch(logout());
+    }
+    dispatch(setAlert(message, "error"));
+    dispatch({ type: GET_SCORES_REQUEST_ERROR, payload: message });
+  }
+};
+
+export const getScores = (roomID) => async (dispatch) => {
+  try {
+    dispatch({ type: GET_SCORES_REQUEST });
+
+    const { data } = await api.get(`/scores?roomID=${roomID}`);
+
+    dispatch({ type: GET_SCORES_REQUEST_SUCCESS, payload: data });
   } catch (error) {
     const message =
       error.response && error.response.data.message
--- src/reducers/score.js
+++ src/reducers/score.js
@@ -1,30 +1,43 @@
 import {
-   GIVE_SCORE_REQUEST,
-   GIVE_SCORE_REQUEST_ERROR,
-   GIVE_SCORE_REQUEST_SUCCESS,
-  } from "../constants/scoreConstant";
+  GIVE_SCORE_REQUEST,
+  GIVE_SCORE_REQUEST_ERROR,
+  GIVE_SCORE_REQUEST_SUCCESS,
+  GET_SCORES_REQUEST,
+  GET_SCORES_REQUEST_ERROR,
+  GET_SCORES_REQUEST_SUCCESS,
+} from "../constants/scoreConstant";
 
-  const initialState = {
-    scores: [],
-    loading: false,
-    error: null,
-  };
-  
-  export const scoreReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case GIVE_SCORE_REQUEST:
-        return { ...state, loading: true };
-      case GIVE_SCORE_REQUEST_SUCCESS:
-        return {
-          ...state,
-          loading: false,
-          scores: [...state.scores, action.payload],
-        };
-      case GIVE_SCORE_REQUEST_ERROR:
-        return { ...state, loading: false, error: action.payload };
-      
-      default:
-        return state;
-    }
-  };
-  
+const initialState = {
+  scores: [],
+  loading: false,
+  error: null,
+};
+
+export const scoreReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GIVE_SCORE_REQUEST:
+      return { ...state, loading: true };
+    case GIVE_SCORE_REQUEST_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        scores: [...state.scores, action.payload],
+      };
+    case GIVE_SCORE_REQUEST_ERROR:
+      return { ...state, loading: false, error: action.payload };
+
+    case GET_SCORES_REQUEST:
+      return { ...state, loading: true };
+    case GET_SCORES_REQUEST_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        scores: action.payload,
+      };
+    case GET_SCORES_REQUEST_ERROR:
+      return { ...state, loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+};
