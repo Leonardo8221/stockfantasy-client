59d8d07e2260f60a52e3cd4ceb15a023c55c21df
Leonardo8221 <r.leonardo0802@gmail.com>
2023-06-29 22:26:48 +0300

    Update StockListItem

--- src/components/commons/StockListItem/index.js
+++ src/components/commons/StockListItem/index.js
@@ -2,13 +2,13 @@ import "./style.scss"
 
 const StockListItem = (props) => {
 
-    const {symbol, price, exchange, onClick} = props;
+    const {symbol, price, name, onClick} = props;
     return (
         <div className="stock-list-item" onClick={onClick}>
             <h5 className="stock-list-item-name">{symbol}</h5>
             <div className="text-muted stock-list-item-info">
                 <p className="stock-list-item-info-price">Price: {price}</p>
-                <p className="stock-list-item-info-date">ExChange: {exchange}</p>
+                <p className="stock-list-item-info-date">Name: {name}</p>
             </div>
         </div>
     )
